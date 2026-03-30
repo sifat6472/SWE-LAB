a2d67752dfb62ef3e89af5ef2ef2d3f763e687d0
juhaer-10 <kakhtab222112@bscse.uiu.ac.bd>
2026-03-30 18:08:46 +0600

    Juhaer: added function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ print("Team Collaboration Project Started")
 def dev1():
     return "Hello from Dev 1"
 
+def juhaer():
+    return "Hello from Juhaer"
+
 
 if __name__ == "__main__":
     print(dev1())
+    print(juhaer())
